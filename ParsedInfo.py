5a49e35ab2938124f7f3c7f2c9b2cb09e018952e
t-a-y-NY <Temuchin.Yousafi@neulion.local>
2017-07-31 11:07:12 -0400

    Changed Collection
    Changed Collection and added av_loader

---
+++ ParsedInfo.py
@@ -0,0 +1,53 @@
+from datetime import timedelta
+import datetime
+import av_loader
+
+class ParsedInfo:
+    def __init__(self, info, date):
+        dateStr = str(date)
+        self.info = info
+        self.volume = float(self.info["Time Series (Daily)"][dateStr]["5. volume"])
+        self.open = float(self.info["Time Series (Daily)"][dateStr]["1. open"])
+        self.close = float(self.info["Time Series (Daily)"][dateStr]["4. close"])
+        self.percentChange = (self.close - self.open)/self.open
+
+        mostRecentDate = info["Meta Data"]["3. Last Refreshed"]
+        self.currentPrice = info["Time Series (Daily)"][mostRecentDate]["4. close"]
+
+        self.__moving_average(self.info)
+
+    def __moving_average(self, res):
+        current_day = datetime.date.today()
+        days_counted = 0
+        total = 0
+        self.mavg_50 = 0
+        self.mavg_100 = 0
+        self.mavg_200 = 0
+        while days_counted < 200:
+            # do something
+
+            current_day = self.__next_day_back(res, current_day)
+            # current_day = current_day - datetime.timedelta(1)
+
+            total = total + float(res['Time Series (Daily)'][str(current_day)]['4. close'])
+            days_counted = days_counted + 1
+            if days_counted == 50:
+                self.mavg_50 = total / 50
+            elif days_counted == 100:
+                self.mavg_100 = total / 100
+            elif days_counted == 200:
+                self.mavg_200 = total / 200
+        # return mavg_50, mavg_100, mavg_200
+
+    def __next_day_back(self, res, curr):
+        now = curr - datetime.timedelta(1)
+        counter = 0
+        while not str(now) in res['Time Series (Daily)']:
+            if counter > 200:
+                raise av_loader.TimeoutException("Timeout occured trying to fetch next day back")
+            now = now - datetime.timedelta(1)
+            counter = counter + 1
+        return now
+
+    def __str__(self):
+        print(self.volume)

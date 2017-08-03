9fb5c12bcf6820c765cb3dc3b6589cf453f167a1
Evan Zou <evanjzou@gmail.com>
2017-08-03 14:57:25 -0400

    Cleaned up Collection with StockDataCollection and factored into avloading folder

---
+++ avloading/StockDataCollection.py
@@ -0,0 +1,156 @@
+"""Module handles the construction of time series objects
+for stock data
+"""
+import datetime
+import av_loader
+
+class StockTimeSeries:
+    """A Collection of stock time instances
+    for all time points where data is available
+    """
+    def __init__(self, company_name, time_diff=None):
+        if time_diff != None:
+            raise NotImplementedError("Only daily time differential implemented")
+        self.data_loader = av_loader.AVLoader(company_name, function=av_loader.AVFunction.DAILY)
+        stock_data = self.data_loader.get_stock_data()
+        self.time_diff = time_diff
+        self.today = TimeInstance(stock_data, str(datetime.date.today()), is_today=True)
+        self.series = []
+        self.__set_series(stock_data)
+
+    def __set_series(self, stock_data):
+        """Populate series with time instances"""
+        if self.time_diff != None:
+            raise NotImplementedError("Only daily time differential implemented")
+        current_day = None
+
+        try:
+            current_day = next_day_back(stock_data, datetime.date.today())
+        except av_loader.TimeoutException:
+            raise IndexError("Stock data is not valid")
+
+        while True:
+            self.series.append(TimeInstance(stock_data, str(current_day)))
+            try:
+                current_day = next_day_back(stock_data, current_day)
+            except av_loader.TimeoutException:
+                break
+        self.series.reverse()
+
+    def __str__(self):
+        return "Series of " + str(len(self.series)) + " trading days"
+
+class TimeInstance:
+    """Represents a single instance of time"""
+
+    def __init__(self, stock_data, time, interval=None, is_today=False):
+        self.info = StockInfo(time, stock_data, is_today)
+        self.interval = interval
+        self.prev = None
+
+    def set_prev(self, prev):
+        """Sets the previous time instance to prev"""
+        self.prev = prev
+
+    def __str__(self):
+        return str(self.info)
+
+class StockInfo:
+    """Represents information about this stock at a given instance of time"""
+
+    def __init__(self, time, stock_data, is_today=False):
+
+        most_recent = time
+        if is_today:
+            most_recent = stock_data["Meta Data"]["3. Last Refreshed"]
+        date = date_from_time(most_recent)
+        now_info = stock_data['Time Series (Daily)'][most_recent]
+        self.open = float(now_info["1. open"])
+        self.high = float(now_info["2. high"])
+        self.low = float(now_info["3. low"])
+        self.close = float(now_info["4. close"])
+        self.volume = float(now_info["5. volume"])
+
+        mavg_50, mavg_100, mavg_200 = moving_average(
+            stock_data, datetime.date(int(date[:4]), int(date[5:7]), int(date[8:])))
+        self.mavg_50 = mavg_50
+        self.mavg_100 = mavg_100
+        self.mavg_200 = mavg_200
+
+        day10vol, day90vol = volume_avg(
+            stock_data, datetime.date(int(date[:4]), int(date[5:7]), int(date[8:])))
+        self.volume_10day = day10vol
+        self.volume_3month = day90vol
+
+    def __str__(self):
+        return str((self.open, self.high, self.low, self.close,
+                    self.volume, self.mavg_50, self.mavg_100, self.mavg_200,
+                    self.volume_10day, self.volume_3month))
+
+def date_from_time(time):
+    """Returns the date of a time string that begins with format
+    YYYY-MM-DD
+    """
+    return time[0:10]
+
+def next_day_back(res, curr):
+    """Returns the latest day before curr with stock trading data
+    """
+    now = curr - datetime.timedelta(1)
+    counter = 0
+    while not str(now) in res['Time Series (Daily)']:
+        if counter > 200:
+            raise av_loader.TimeoutException("Timeout occured trying to fetch next day back")
+        now = now - datetime.timedelta(1)
+        counter = counter + 1
+    return now
+
+def moving_average(res, today, is_today=False):
+    """return the 50, 100, and 200 day moving averages"""
+    current_day = today
+    days_counted = 0
+    total = 0
+    if is_today:
+        total = total + float(
+            res['Time Series (Daily)'][res["Meta Data"]["3. Last Refreshed"]]['4. close'])
+        days_counted = days_counted + 1
+    elif str(today) in res['Time Series (Daily)']:
+        total = total + float(res['Time Series (Daily)'][str(today)]['4. close'])
+        days_counted = days_counted + 1
+    mavg_50 = 0
+    mavg_100 = 0
+    mavg_200 = 0
+    while days_counted < 200:
+        try:
+            current_day = next_day_back(res, current_day)
+        except av_loader.TimeoutException:
+            break
+        total = total + float(res['Time Series (Daily)'][str(current_day)]['4. close'])
+        days_counted = days_counted + 1
+        if days_counted == 50:
+            mavg_50 = total / 50
+        elif days_counted == 100:
+            mavg_100 = total / 100
+        elif days_counted == 200:
+            mavg_200 = total / 200
+    return mavg_50, mavg_100, mavg_200
+
+def volume_avg(res, today):
+    """return the 10 day and 90 day volume averages"""
+    current_day = today
+    days_counted = 0
+    total = 0
+    day10vol = 0
+    day90vol = 0
+    while days_counted < 90:
+        try:
+            current_day = next_day_back(res, current_day)
+        except av_loader.TimeoutException:
+            break
+        total = total + float(res['Time Series (Daily)'][str(current_day)]["5. volume"])
+        days_counted = days_counted + 1
+        if days_counted == 10:
+            day10vol = total / 10
+        elif days_counted == 90:
+            day90vol = total / 90
+    return day10vol, day90vol
